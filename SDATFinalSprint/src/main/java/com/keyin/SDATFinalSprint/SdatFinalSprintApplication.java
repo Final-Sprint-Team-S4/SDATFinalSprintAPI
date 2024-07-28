ab7b3cfed84d74114fb6160a567533e9535d8e7e
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-07-28 19:12:50 -0230

    Add files via upload

---
+++ SDATFinalSprint/src/main/java/com/keyin/SDATFinalSprint/SdatFinalSprintApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.SDATFinalSprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdatFinalSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdatFinalSprintApplication.class, args);
+	}
+
+}
---
+++ SDATFinalSprint/src/test/java/com/keyin/SDATFinalSprint/SdatFinalSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.SDATFinalSprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdatFinalSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
