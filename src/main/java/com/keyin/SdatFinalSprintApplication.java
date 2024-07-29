f498a37e40749f48a342a3b1c13653bbbb08a7ea
MohamedMaghrebi <138633077+MohamedMaghrebi@users.noreply.github.com>
2024-07-29 14:14:27 -0230

    Commit of refactor

---
+++ src/main/java/com/keyin/SdatFinalSprintApplication.java
@@ -0,0 +1,13 @@
+package com.keyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdatFinalSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdatFinalSprintApplication.class, args);
+	}
+
+}
