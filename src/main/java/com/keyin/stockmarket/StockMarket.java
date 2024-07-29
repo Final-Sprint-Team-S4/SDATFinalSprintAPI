c6f86f08ff4309645782b4b2ae957a6e665bf01d
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-07-29 18:41:54 -0230

    createStock_method
    adding createStock method to the stockService as well as adding StockMarketRepository

--- src/main/java/com/keyin/stockmarket/StockMarket.java
+++ src/main/java/com/keyin/stockmarket/StockMarket.java
@@ -1,6 +1,7 @@
 //Mohamed
 package com.keyin.stockmarket;
 
+import com.keyin.stock.Stock;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -13,5 +14,7 @@ public class StockMarket {
     @SequenceGenerator(name = "stockMarket_sequence", sequenceName = "stockMarket_sequence", allocationSize = 1, initialValue=1)
     @GeneratedValue(generator = "stockMarket_sequence")
     private long stockMarketId;
-    
+
+    public void setStock(Stock stock) {
+    }
 }
