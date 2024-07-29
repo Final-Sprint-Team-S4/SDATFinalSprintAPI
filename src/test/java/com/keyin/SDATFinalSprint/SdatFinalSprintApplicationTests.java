927dbb64743018ad38f0e44002ce9444072f4746
MohamedMaghrebi <138633077+MohamedMaghrebi@users.noreply.github.com>
2024-07-29 15:59:04 -0230

    Add

--- src/test/java/com/keyin/SDATFinalSprint/SdatFinalSprintApplicationTests.java
+++ src/test/java/com/keyin/SDATFinalSprint/SdatFinalSprintApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SdatFinalSprintApplicationTests {
+public class SdatFinalSprintApplicationTests {
 
     @Test
     void contextLoads() {
